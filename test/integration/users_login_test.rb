708cbee81ebf00008af376b1aa665f6b4adf3e1f
faith-holden <ha.av.sodak@gmail.com>
2021-12-20 13:11:53 -0600

    improved log in and log in testing

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,10 +17,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+
+  test "log in with remembering" do
+     log_in_as(@user, remember_me: '1')
+     assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
+
   test "valid login followed by logout" do
     get login_path
-    post login_path, params: {session: { email: @user.email, password: 'password'}}
-    
+    log_in_as(@user, remember_me: '1')
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -31,6 +44,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+
+    delete logout_path #simulates user clicking logout again in another window
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,9 @@ Minitest::Reporters.use!
 
 
 class ActiveSupport::TestCase
+  #SUPER IMPORTANT TO NOT USE THIS!!! (Tests run in parallel otherwise)
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+  # parallelize(workers: :number_of_processors, with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
@@ -21,4 +22,16 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: {session: { email: user.email,
+                                         password: password,
+                                         remember_me: remember_me } }
+  end
 end
